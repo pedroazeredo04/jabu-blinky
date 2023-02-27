57e224f96a764f059ba41618faec357c350fb643
pedroazeredo04 <89558427+pedroazeredo04@users.noreply.github.com>
2023-02-27 17:30:25 -0300

    Initial commit

---
+++ tests/bin/test_main.c
@@ -0,0 +1,15 @@
+/**
+ * @file test_main.c
+ *
+ * @brief Main function for tests.
+ */
+
+#include <stdio.h>
+#include "tests_core.h"
+
+int main(void) {
+    test_core_init();
+
+    for (;;) {
+    }
+}
