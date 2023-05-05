5ea97c0f31c65a42bf5bd7da85ee266d968f4a85
pedroazeredo04 <MY_NAME@example.com>
2023-05-05 16:46:01 -0300

    VAMO GIGANTE (ainda falta piscar)

--- src/main.c
+++ src/main.c
@@ -18,9 +18,22 @@
 
 int main(void) {
     mcu_init();
+    led_toggle();
+    led_toggle();
+    MX_FREERTOS_Init();
+    osKernelStart();
 
-    for (;;) {
-        led_toggle();
-        mcu_sleep(LED_TOGGLE_DELAY_MS);
-    }
+    for (;;);
+}
+
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    osDelay(500);
+  }
+  /* USER CODE END StartDefaultTask */
 }
